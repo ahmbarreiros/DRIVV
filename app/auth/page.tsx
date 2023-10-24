6dc98a397ca2db1b17cd9050db64d2158facb447
Antonio <ahmbarreiros.cs@gmail.com>
2023-10-24 18:44:34 -0300

    initial

---
+++ app/auth/page.tsx
@@ -0,0 +1,13 @@
+const Auth = () => {
+    return (
+        <div className="relative h-full w-full bg-[url('/images/background.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo.png" alt="logo" className="h-48" />
+                </nav>
+                <div className=""></div>
+            </div>
+        </div>
+    );
+};
+export default Auth;
