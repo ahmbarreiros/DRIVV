63bfd78bb2d83ba4346dfc973b079eafaefff359
Antonio <ahmbarreiros.cs@gmail.com>
2023-10-27 19:11:28 -0300

    register state

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { SetStateAction, useState } from "react";
+import { SetStateAction, useCallback, useState } from "react";
 import Input from "@/components/Input";
 
 const Auth = () => {
@@ -7,31 +7,44 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const [account, setAccount] = useState("login");
+
+    const toggleAccount = useCallback(() => {
+        setAccount((currentState) =>
+            currentState == "login" ? "register" : "login"
+        );
+    }, []);
+
     return (
         <div className="relative h-full w-full bg-[url('/images/background.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             {/* TODO(alb): change opacity on large screens?*/}
-            <div className="bg-black w-full h-full bg-opacity-70">
+            <div className="bg-black w-full h-full lg:bg-opacity-70">
                 <nav className="px-12 py-5">
                     <img src="/images/logo.png" alt="logo" className="h-48" />
                 </nav>
                 <div className="flex justify-center">
                     {/* TODO(alb): change width on large screens?*/}
-                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 w-2/5 max-w-md rounded-md w-full">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font-semibold">
-                            Sign in
+                            {account == "login" ? "Sign in" : "Register"}
                         </h2>
+
                         <div className="flex flex-col gap-4">
-                            <Input
-                                label="Username"
-                                onChange={(event: {
-                                    /* quick fix for parameter of type any*/
-                                    target: { value: SetStateAction<string> };
-                                }) => {
-                                    setUsername(event.target.value);
-                                }}
-                                id="username"
-                                value={username}
-                            />
+                            {account == "register" && (
+                                <Input
+                                    label="Username"
+                                    onChange={(event: {
+                                        /* quick fix for parameter of type any*/
+                                        target: {
+                                            value: SetStateAction<string>;
+                                        };
+                                    }) => {
+                                        setUsername(event.target.value);
+                                    }}
+                                    id="username"
+                                    value={username}
+                                />
+                            )}
                             <Input
                                 label="Email"
                                 onChange={(event: {
@@ -56,8 +69,21 @@ const Auth = () => {
                             />
                         </div>
                         <button className="bg-cyan-600 py-3 text-white rounded-md w-full mt-10 hover:bg-cyan-700 transition">
-                            Login
+                            {account == "login" ? "Login" : "Sign up"}
                         </button>
+                        <p className="text-neutral-500 mt-12">
+                            {account == "login"
+                                ? "First time using Drivv?"
+                                : "Already have an account?"}
+                            <span
+                                onClick={toggleAccount}
+                                className="text-white ml-1 hover:underline cursor-pointer"
+                            >
+                                {account == "login"
+                                    ? "Create an account"
+                                    : "Sign in"}
+                            </span>
+                        </p>
                     </div>
                 </div>
             </div>
