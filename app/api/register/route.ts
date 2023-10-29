6003be9db35a28133a5c04e6126a94ea02f973c8
Antonio <ahmbarreiros.cs@gmail.com>
2023-10-28 23:10:34 -0300

    register authentication api

---
+++ app/api/register/route.ts
@@ -0,0 +1,50 @@
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/prismadb";
+
+export async function POST(req: Request) {
+    try {
+        const data = await req.json();
+        const { username, email, password } = data;
+
+        //   TODO(alb): handle username/email/password undefined better later.
+        if (typeof email !== "string")
+            return NextResponse.json(
+                { error: "Invalid email" },
+                { status: 422 }
+            );
+
+        const duplicateUser = await prismadb.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
+
+        if (duplicateUser) {
+            console.log("duplicate user");
+
+            return NextResponse.json(
+                { error: "Email already taken." },
+                { status: 422 }
+            );
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 16);
+
+        const user = await prismadb.user.create({
+            data: {
+                email,
+                username,
+                hashedPassword,
+                image: "",
+                emailVerified: new Date(),
+            },
+        });
+
+        return NextResponse.json(user, { status: 200 });
+    } catch (error) {
+        // TODO(alb): Change this for prod!
+        console.log(error);
+        return NextResponse.json(error, { status: 400 });
+    }
+}
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { SetStateAction, useCallback, useState } from "react";
 import Input from "@/components/Input";
+import axios from "axios";
+import { log } from "console";
+import { signIn } from "next-auth/react";
 
 const Auth = () => {
     const [username, setUsername] = useState("");
@@ -15,6 +18,19 @@ const Auth = () => {
         );
     }, []);
 
+    const register = useCallback(async () => {
+        try {
+            await axios.post("/api/register", {
+                email,
+                username,
+                password,
+            });
+        } catch (error) {
+            // TODO(alb): change this for production!
+            console.log(error);
+        }
+    }, [email, username, password]);
+
     return (
         <div className="relative h-full w-full bg-[url('/images/background.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             {/* TODO(alb): change opacity on large/medium screens?*/}
@@ -24,7 +40,7 @@ const Auth = () => {
                 </nav>
                 <div className="flex justify-center">
                     {/* TODO(alb): change width on large screens?*/}
-                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md md:w-3/5 rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font-semibold">
                             {account == "login" ? "Sign in" : "Register"}
                         </h2>
@@ -33,12 +49,7 @@ const Auth = () => {
                             {account == "register" && (
                                 <Input
                                     label="Username"
-                                    onChange={(event: {
-                                        /* quick fix for parameter of type any*/
-                                        target: {
-                                            value: SetStateAction<string>;
-                                        };
-                                    }) => {
+                                    onChange={(event: any) => {
                                         setUsername(event.target.value);
                                     }}
                                     id="username"
@@ -47,28 +58,27 @@ const Auth = () => {
                             )}
                             <Input
                                 label="Email"
-                                onChange={(event: {
-                                    /* quick fix for parameter of type any*/
-                                    target: { value: SetStateAction<string> };
-                                }) => {
+                                onChange={(event: any) => {
                                     setEmail(event.target.value);
                                 }}
                                 id="email"
+                                type="email"
                                 value={email}
                             />
                             <Input
                                 label="Password"
-                                onChange={(event: {
-                                    /* quick fix for parameter of type any*/
-                                    target: { value: SetStateAction<string> };
-                                }) => {
+                                onChange={(event: any) => {
                                     setPassword(event.target.value);
                                 }}
                                 id="password"
+                                type="password"
                                 value={password}
                             />
                         </div>
-                        <button className="bg-cyan-600 py-3 text-white rounded-md w-full mt-10 hover:bg-cyan-700 transition">
+                        <button
+                            onClick={register}
+                            className="bg-cyan-600 py-3 text-white rounded-md w-full mt-10 hover:bg-cyan-700 transition"
+                        >
                             {account == "login" ? "Login" : "Sign up"}
                         </button>
                         <p className="text-neutral-500 mt-12">
